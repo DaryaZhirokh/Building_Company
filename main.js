340a8e5f74a1acb9999402f907d910ab94bb9c70
Darya <daraziroh@gmail.com>
2020-01-17 11:01:08 +0300

    js added letting more information refs

---
+++ main.js
@@ -0,0 +1,11 @@
+/*letting read more*/
+
+document.addEventListener('click', function(event) {
+    let id = event.target.dataset.toggleId;
+    if (!id) return;
+
+    let elem = document.getElementById(id);
+
+    elem.hidden = !elem.hidden;
+  });
+
