b89c2c1f68003e5b8ef8ed32e4febb35d99859db
Darya <daraziroh@gmail.com>
2020-01-17 14:42:57 +0300

    js added showingMoreNews

--- main.js
+++ main.js
@@ -3,9 +3,15 @@
 document.addEventListener('click', function(event) {
     let id = event.target.dataset.toggleId;
     if (!id) return;
-
     let elem = document.getElementById(id);
-
     elem.hidden = !elem.hidden;
   });
 
+let watchAll = document.getElementById("watchAll")
+let newsBlockMore = document.getElementById("newsBlockMore")
+
+watchAll.addEventListener('click', function () {
+  /*newsBlockMore.hidden = !newsBlockMore.hidden;*/
+  newsBlockMore.style.display = "block";
+  watchAll.style.display = "none";
+},);
